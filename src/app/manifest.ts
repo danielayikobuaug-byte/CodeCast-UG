695258ca96df5404a00d05e106c1c0b63e95f1f4
Daniel Ayikobua <danielayikobua.ug@gmail.com>
2026-08-15 23:43:56 +0300

    updated the favicon and changed the manifest urls

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#0E1D30',
     icons: [
       {
-        src: 'https://i.pinimg.com/736x/2a/ed/42/2aed427a3ca30fbe9661b25b867fc532.jpg',
+        src: 'https://i.pinimg.com/736x/b3/2d/58/b32d58f94b4dbdf83307e0844238c6f6.jpg',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: 'https://i.pinimg.com/736x/2a/ed/42/2aed427a3ca30fbe9661b25b867fc532.jpg',
+        src: 'https://i.pinimg.com/736x/b3/2d/58/b32d58f94b4dbdf83307e0844238c6f6.jpg',
         sizes: '512x512',
         type: 'image/png',
       },
