e7400cea5bba835f6f6dec1e63d880aefa0df6cf
Daniel Ayikobua <danielayikobua.ug@gmail.com>
2026-08-15 23:13:59 +0300

    updates in the manifest icons

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#0E1D30',
     icons: [
       {
-        src: 'https://picsum.photos/seed/codecast-icon/192/192',
+        src: 'https://i.pinimg.com/736x/2a/ed/42/2aed427a3ca30fbe9661b25b867fc532.jpg',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: 'https://picsum.photos/seed/codecast-icon-large/512/512',
+        src: 'https://i.pinimg.com/736x/2a/ed/42/2aed427a3ca30fbe9661b25b867fc532.jpg',
         sizes: '512x512',
         type: 'image/png',
       },
