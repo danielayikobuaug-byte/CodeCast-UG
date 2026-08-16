65eeeee5bb729143a217c7deceb7101b10d80814
Daniel Ayikobua <danielayikobua.ug@gmail.com>
2026-08-16 21:14:44 +0300

    changed description

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -4,7 +4,7 @@ export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'CodeCast UG LTD',
     short_name: 'CodeCast',
-    description: 'Technology & Entertainment Solutions in Kampala, Uganda',
+    description: 'TV and IT Solutions in Uganda',
     start_url: '/',
     display: 'standalone',
     background_color: '#0E1D30',
